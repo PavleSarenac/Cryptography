bbbb57d2659502e2760a4c6db057ed81e803b4ad
Pavle Sarenac <sp200359d@student.etf.bg.ac.rs>
2024-04-09 12:38:24 +0200

    RSA algorithm implemented.

--- 18_rsa_algorithm.py
+++ 18_rsa_algorithm.py
@@ -9,14 +9,14 @@ n = None
 
 
 def is_prime_number_miller_rabin_algorithm(p):
-    k, q = get_k_and_q(p - 1)
-    a = get_a(p)
-    if is_first_condition_true(a, q, p) or is_second_condition_true(a, k, q, p):
+    k, q = miller_rabin_get_k_and_q(p - 1)
+    a = miller_rabin_get_a(p)
+    if miller_rabin_is_first_condition_true(a, q, p) or miller_rabin_is_second_condition_true(a, k, q, p):
         return True
     return False
 
 
-def get_k_and_q(decremented_number):
+def miller_rabin_get_k_and_q(decremented_number):
     k = 0
     current_number = decremented_number
     while current_number % 2 == 0:
@@ -26,19 +26,32 @@ def get_k_and_q(decremented_number):
     return k, q
 
 
-def get_a(p):
+def miller_rabin_get_a(p):
     return random.randint(2, p - 2)
 
 
-def is_first_condition_true(a, q, p):
+def miller_rabin_is_first_condition_true(a, q, p):
     return pow(a, q, p) == 1
 
 
-def is_second_condition_true(a, k, q, p):
+def miller_rabin_is_second_condition_true(a, k, q, p):
     for j in range(k):
         if pow(a, pow(2, j) * q, p) == p - 1:
             return True
     return False
+
+
+def rsa_get_p_and_q():
+    p = q = None
+    for potential_factor_of_n in range(1 << 6, 1 << 7):
+        if is_prime_number_miller_rabin_algorithm(potential_factor_of_n):
+            p = potential_factor_of_n
+            break
+    for potential_factor_of_n in range(1 << 6, 1 << 7):
+        if potential_factor_of_n != p and is_prime_number_miller_rabin_algorithm(potential_factor_of_n):
+            q = potential_factor_of_n
+            break
+    return p, q
 
 
 def rsa_get_e(phi_n):
@@ -48,17 +61,30 @@ def rsa_get_e(phi_n):
             return potential_e
 
 
-def rsa_get_p_and_q():
-    p = q = None
-    for potential_factor_of_n in range(1 << 1024, 1 << 1025):
-        if is_prime_number_miller_rabin_algorithm(potential_factor_of_n):
-            p = potential_factor_of_n
-            break
-    for potential_factor_of_n in range(1 << 1024, 1 << 1025):
-        if potential_factor_of_n != p and is_prime_number_miller_rabin_algorithm(potential_factor_of_n):
-            q = potential_factor_of_n
-            break
-    return p, q
+def print_info(p, q, phi_n, d):
+    global n, e
+    print(f"p = {p};")
+    print(f"q = {q};")
+    print(f"n = p * q = {n};")
+    print(f"phi_n = phi_p * phi_q = (p - 1) * (q - 1) = {phi_n};")
+    print(f"e = {e};")
+    print(f"d = {d};")
+
+
+def encrypt(plain_text):
+    global n, e
+    cipher_text = []
+    for char in plain_text:
+        cipher_text.append(pow(alphabet.index(char), e, n))
+    return cipher_text
+
+
+def decrypt(cipher_text, d):
+    global n
+    plain_text = []
+    for num in cipher_text:
+        plain_text.append(alphabet[pow(num, d, n)])
+    return "".join(plain_text)
 
 
 def main():
@@ -68,6 +94,11 @@ def main():
     phi_n = (p - 1) * (q - 1)
     e = rsa_get_e(phi_n)
     d = pow(e, -1, phi_n)
+    print_info(p, q, phi_n, d)
+    cipher_text = encrypt("desfirujovo")
+    plain_text = decrypt(cipher_text, d)
+    print(plain_text)
+    print(cipher_text)
 
 
 if __name__ == "__main__":
